ee98bd3372d5743cf63d34d482525430d9bb4e06
Arafat Azam <arafatazam17@gmail.com>
2022-12-19 23:19:39 +0000

    Data set changed

--- solution.py
+++ solution.py
@@ -9,21 +9,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def co2_emission_barchart(df: pd.DataFrame):
-    df = df.loc["CO2 emissions (metric tons per capita)"]
+def greenhouse_gas_emission_barchart(df: pd.DataFrame):
+    df = df.loc["Total greenhouse gas emissions (kt of CO2 equivalent)"]
     years = ["1990", "1995", "2000", "2005", "2010", "2015"]
     df = df[years]
     ax = df.plot(kind='bar', rot=20)
-    ax.set_title('CO2 Emission')
+    ax.set_title('Total greenhouse gas emissions')
     ax.set_xlabel('Countries')
-    ax.set_ylabel('Metric ton per capita')
+    ax.set_ylabel('kt of CO2 equivalent')
     ax.get_figure().set_size_inches(6, 5)
     plt.show()
 
 
 def main():
     df = pd.read_excel('wb_data.xlsx', index_col=[0, 1])
-    co2_emission_barchart(df)
+    greenhouse_gas_emission_barchart(df)
 
 
 if __name__ == "__main__":
